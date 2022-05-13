fb41af7902c3fea6bae3875232c5c6c172b22bb2
Fabio Monzani <renderbros@gmail.com>
2022-05-13 12:09:21 +0100

    Added figma classes for dark mode

--- code.js
+++ code.js
@@ -3,7 +3,7 @@
 // full browser environment (see documentation).
 
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__, {width: 360, height: 640})
+figma.showUI(__html__, {themeColors: true, width: 360, height: 640})
 
 // Function for loading all the needed fonts
 const loadFonts = async () => {
