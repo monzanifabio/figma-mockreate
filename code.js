83b994b879b3671942f94f20ebae04e0808cd875
monzanifabio <renderbros@gmail.com>
2023-09-30 01:49:47 +0100

    Testing logic changes

--- code.js
+++ code.js
@@ -3,55 +3,80 @@
 // full browser environment (see documentation).
 
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__, {themeColors: true, width: 360, height: 640})
+figma.showUI(__html__, { themeColors: true, width: 360, height: 640 });
 
 // Function for loading all the needed fonts
 const loadFonts = async () => {
-  await figma.loadFontAsync({ family: "Inter", style: "Regular" })
-  console.log("Awaiting the fonts.")
-}
+  await figma.loadFontAsync({ family: "Inter", style: "Regular" });
+  console.log("Awaiting the fonts.");
+};
 // Load the fonts
 loadFonts().then(() => {
-  console.log("Fonts fetched and ready to go!")
-})
+  console.log("Fonts fetched and ready to go!");
+});
 
-figma.ui.onmessage = msg => {
-  if (msg.type === 'create-random-text') {
+figma.on("selectionchange", () => {
+  detectSelection();
+});
+
+function detectSelection() {
+  const selection = figma.currentPage.selection;
+  if (selection.length) {
+    figma.ui.postMessage({ type: selection.length, selected: true });
+    // for (let i = 0; i < selection.length; i++) {
+    //   if (selection[i].type == "TEXT") {
+    //     console.log(selection[i]);
+    //     console.log(selection[i].characters);
+    //   }
+    // }
+  } else {
+    figma.ui.postMessage({ selected: false });
+    console.log("No selection");
+  }
+}
+
+figma.ui.onmessage = (msg) => {
+  if (msg.type === "create-random-text") {
     for (var i = 0; i < msg.genStorage.length; i++) {
-      console.log(msg.genStorage[i])
+      console.log(msg.genStorage[i]);
     }
 
     const nodes = [];
     let spacing = 0;
 
     //Create the table header if existing
-    if (msg.colHeader != "") {
-      const header = figma.createText();
-      header.fontSize = msg.fontSize;
-      header.characters = msg.colHeader;
-      header.x = figma.viewport.center.x;
-      header.y = msg.textSpacing - (msg.textSpacing * 2) - msg.fontSize;
-      nodes.push(header);
-    }
-    
+    // if (msg.colHeader != "") {
+    //   const header = figma.createText();
+    //   header.fontSize = msg.fontSize;
+    //   header.characters = msg.colHeader;
+    //   header.x = figma.viewport.center.x;
+    //   header.y = msg.textSpacing - msg.textSpacing * 2 - msg.fontSize;
+    //   nodes.push(header);
+    // }
+
+    const selection = figma.currentPage.selection;
+    const count = selection.length;
+
     //Loop through the text nodes
-    for (let i = 0; i < msg.count; i++) {
-      const text = figma.createText();
-      text.fontSize = msg.fontSize;
-      text.x = figma.viewport.center.x
-      text.y = spacing;
-      text.characters = msg.genStorage[i]
-      figma.currentPage.appendChild(text);
-      nodes.push(text);
-      //Get current text height
-      let textHeight = text.height;
-      //Add spacing in between text elements
-      spacing += msg.textSpacing + textHeight
+    for (let i = 0; i < count; i++) {
+      console.log(msg.genStorage[i]);
+      selection[i].characters = msg.genStorage[i];
+      // const text = figma.createText();
+      // text.fontSize = msg.fontSize;
+      // text.x = figma.viewport.center.x;
+      // text.y = spacing;
+      // text.characters = msg.genStorage[i];
+      // figma.currentPage.appendChild(text);
+      // nodes.push(text);
+      // //Get current text height
+      // let textHeight = text.height;
+      // //Add spacing in between text elements
+      // spacing += msg.textSpacing + textHeight;
     }
-    figma.currentPage.selection = nodes;
-    figma.group(figma.currentPage.selection, figma.currentPage)
-    figma.viewport.scrollAndZoomIntoView(nodes);
-    figma.notify("Data generated")
+    // figma.currentPage.selection = nodes;
+    // figma.group(figma.currentPage.selection, figma.currentPage);
+    // figma.viewport.scrollAndZoomIntoView(nodes);
+    figma.notify("Data generated");
   }
 
   // Make sure to close the plugin when you're done. Otherwise the plugin will
