97a1553f553a107725a7e1886614a54d9a331b77
monzanifabio <renderbros@gmail.com>
2023-10-02 08:22:26 +0100

    Fixed a bug that prevented multiple fonts to be edited at once

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
 // This shows the HTML page in "ui.html".
 figma.showUI(__html__, { themeColors: true, width: 320, height: 400 });
 
-// Function for loading all the needed fonts
+// Function for loading starter fonts
 const loadFonts = async () => {
   await figma.loadFontAsync({ family: "Inter", style: "Regular" });
   console.log("Awaiting the fonts.");
@@ -15,15 +15,17 @@ loadFonts().then(() => {
 detectSelection();
 
 // Trigger on selection change
-figma.on("selectionchange", () => {
+figma.on("selectionchange", async () => {
   detectSelection();
 });
 
 // Check if the selection is of type text
-function detectSelection() {
+async function detectSelection() {
   const selection = figma.currentPage.selection;
+  console.log(selection);
   if (selection.length) {
     for (let i = 0; i < selection.length; i++) {
+      await figma.loadFontAsync(selection[i].fontName);
       if (selection[i].type == "TEXT") {
         figma.ui.postMessage({ type: selection.length, selected: true });
       }
